c1e97cdcf066e651dfdf66b55ad449c4e3d22c2c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-07 19:05:06 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -21,30 +21,33 @@ let constants = {
     }
 };
 
-module.exports =
-        Object.freeze(constants);
+export default Object.freeze(constants);
         
+//---
+    //https://github.com/platy/cooking-converter/blob/master/src/units.ts
+    //https://github.com/platy/cooking-converter/blob/master/src/measurement.ts
+    
+export const CONST_TEASPOON = 0.005;
+export const CONST_DESSERTSPOON = 0.010;
+export const CONST_TABLESPOON = 0.015; // except Australia
+export const CONST_FLOZ_US = 0.030;
+export const CONST_FLOZ = 0.02841;
+export const CONST_CUP = 0.250;
+export const CONST_CUPUK = 0.2841;
+export const CONST_CUPUS = 0.240;
+export const CONST_PINT = 0.56826;
+export const CONST_PINT_AUS = 0.570;
+export const CONST_PINT_US = 0.47318;
+export const CONST_QUART = 1.13652;
+export const CONST_QUART_US = 0.94635;
+export const CONST_GALLON = 4.54609;
+export const CONST_GALLON_US = 3.78541;
 
 
 
 
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-const CONST_xxx = ;
-
-
-// for us measurements, those specified by the FDA are used
-// many more us measurements available at https://en.wikipedia.org/wiki/Cooking_weights_and_measures
+// for US measurements, those specified by the FDA are used
+// many more US measurements available at https://en.wikipedia.org/wiki/Cooking_weights_and_measures
 export const CustomaryVolumes = { // for english speaking nations
     Teaspoon: VolumeMeasure(0.005),
     Dessertspoon: VolumeMeasure(0.010),
@@ -76,9 +79,13 @@ export const CustomaryMasses = {
 export const All = Object.assign({}, MetricVolumes, CustomaryVolumes, MetricMasses, CustomaryMasses)
 
 export const TopInputs = {
-    FlOz: CustomaryVolumes.FlOz, CupUs: CustomaryVolumes.CupUs, Grams: MetricMasses.Grams, OzInternational: CustomaryMasses.OzInternational,
+    FlOz: CustomaryVolumes.FlOz, 
+    CupUs: CustomaryVolumes.CupUs, 
+    Grams: MetricMasses.Grams, 
+    OzInternational: CustomaryMasses.OzInternational,
 }
 
 export const TopOutputs = {
-    Millilitres: MetricVolumes.Millilitres, Decilitres: MetricVolumes.Decilitres,
+    Millilitres: MetricVolumes.Millilitres,
+    Decilitres: MetricVolumes.Decilitres,
 }
